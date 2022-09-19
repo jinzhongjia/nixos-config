f5f0e7da87f85f0a641ef6f25cf4154c69215f17
jinzhongjia <jinzhongjia@qq.com>
2022-09-19 15:12:19 +0800

    增加自动更新功能

--- system/upgrade.nix
+++ system/upgrade.nix
@@ -1,9 +1,15 @@
 { ... }:
+
 {
   system.autoUpgrade = {
-    enable = false;
+    enable = true;
+    dates = "04:40";
+    rebootWindow = {
+      lower = "01:00";
+      upper = "05:00";
+    };
     # 暂时关闭效果
     #allowReboot = true;
-    #channel = https://nixos.org/channels/unstable;
+    channel = https://mirror.nju.edu.cn/nix-channels/nixos-unstable;
   };
 }
