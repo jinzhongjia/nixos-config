a6480b595c86e3261f001398ce4a645e8fc1b411
jinzhongjia <jinzhongjia@qq.com>
2022-09-19 15:05:24 +0800

    添加pycharm启动自动进入conda环境

--- system/alias.nix
+++ system/alias.nix
@@ -4,5 +4,8 @@
   environment.shellAliases = {
     buildos = "sudo nixos-rebuild switch";
     upgradeos = "sudo nixos-rebuild switch --upgrade";
+
+    # 个人使用
+    pycharm = "conda-shell -e 'pycharm-professional'";
   };
 }
