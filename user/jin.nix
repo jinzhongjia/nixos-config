e63a17a8423737c241be86c81d44f39e8c5ac5fb
jinzhongjia <jinzhongjia@qq.com>
2022-09-23 20:29:53 +0800

    增加neovim所需要的组件

--- user/jin.nix
+++ user/jin.nix
@@ -1,73 +1,95 @@
 { config, pkgs, ... }:
 
 {
-    users.users.jin = {
-      isNormalUser = true;
-      description = "金中甲";
-      extraGroups = [ "networkmanager" "wheel" ];
-      shell = pkgs.zsh;
-      packages = with pkgs; [
-        firefox
-        
-        google-chrome
-        chromium
+  users.users.jin = {
+    isNormalUser = true;
+    description = "金中甲";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      firefox
 
-        vscode
+      google-chrome
+      chromium
 
-        postman
+      vscode
 
-        dbeaver # 通用数据库可视化工具
+      postman
 
-        jetbrains.goland
-        jetbrains.clion
-        jetbrains.pycharm-professional
-        conda
+      dbeaver # 通用数据库可视化工具
 
-        qtcreator
+      jetbrains.goland
+      jetbrains.clion
+      jetbrains.pycharm-professional
+      conda
 
-        tdesktop
-        discord 
+      qtcreator
 
-        vlc # vedio player
-        deadbeef # audio player
-#       cava
-#       termusic 音乐专用的终端ui
-        calibre # Comprehensive e-book software
-        
-        gimp # 图片编辑工具
+      tdesktop
+      discord
 
-        shotcut # 视频剪辑工具
+      vlc # vedio player
+      deadbeef # audio player
+      #       cava
+      #       termusic 音乐专用的终端ui
+      calibre # Comprehensive e-book software
 
-        libreoffice-qt # kde专用
-        wpsoffice-cn # wpsoffice cn
+      gimp # 图片编辑工具
 
-        vnote
+      shotcut # 视频剪辑工具
 
-        protontricks # proton工具
-        lutris # 游戏整合平台
+      libreoffice-qt # kde专用
+      wpsoffice-cn # wpsoffice cn
 
-        qjournalctl # GUI界面检索及查看系统日志
-        remmina # 远程工具
+      vnote
 
-        obs-studio
+      protontricks # proton工具
+      lutris # 游戏整合平台
 
-        qbittorrent
+      qjournalctl # GUI界面检索及查看系统日志
+      remmina # 远程工具
 
-        bless # 十六进制编辑器
-        ghidra # 逆向工具
+      obs-studio
 
-        lua
+      qbittorrent
 
-        neovim
-        ripgrep
-        fd
+      bless # 十六进制编辑器
+      ghidra # 逆向工具
 
-        nur.repos.linyinfeng.icalingua-plus-plus # incalingua++
+      neovim
+      ripgrep
+      fd
 
-        nur.repos.linyinfeng.wemeet # 腾讯会议
+      # language server
+      gopls # go
+      nodePackages.vls # vue
+      sumneko-lua-language-server # lua
+      ccls # c/c++
+      nodePackages.pyright # python
+      cmake-language-server # cmake
+      nodePackages.bash-language-server # bash
+      nodePackages.dockerfile-language-server-nodejs # dockerfile
+      nodePackages.yaml-language-server # yaml
+      nodePackages.vscode-json-languageserver # json
+      rnix-lsp # nix
 
-      ];
-    };
+      # formatter
+      nodePackages.prettier
+      shfmt # shell
+      stylua # lua
+      gofumpt # go
+      python39Packages.autopep8 # python
+      cmake-format
+      astyle # cpp
+      buf # proto
+      nixfmt # nix
 
-    programs.zsh.enable = true;
+      nur.repos.linyinfeng.icalingua-plus-plus # incalingua++
+
+      nur.repos.linyinfeng.wemeet # 腾讯会议
+
+    ];
+  };
+
+  programs.zsh.enable = true;
 }
