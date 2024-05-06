d5c70b7e21a0b4dc9974479cffd0e88f5e800b4a
jinzhongjia <mail@nvimer.org>
2024-05-06 16:28:14 +0800

    gnome extensions

--- gnome.nix
+++ gnome.nix
@@ -4,4 +4,20 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  
+  # some gnome plugins
+  environment.systemPackages = with pkgs.gnomeExtensions;[
+    blur-my-shell
+    caffeine
+    dock-from-dash
+    lock-keys
+    quick-settings-audio-panel
+    muteunmute
+    vitals
+    coverflow-alt-tab
+    quick-touchpad-toggle
+    tray-icons-reloaded
+    clipboard-indicator
+    no-overview
+  ];
 }
