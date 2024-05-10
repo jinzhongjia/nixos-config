a575b9dcb83726944df9fd7210060da61aca1348
jinzhongjia <mail@nvimer.org>
2024-05-10 19:08:16 +0800

    add direnv support

--- modules/home-manager/tools.nix
+++ modules/home-manager/tools.nix
@@ -29,4 +29,10 @@
   programs.z-lua = {
     enable = true;
   };
+
+  direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
 }
